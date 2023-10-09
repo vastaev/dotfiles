80cb9dcdc3586adb43f074f00363ac63fb126eaa
Вастаев Олег Сергеевич <osvastaev@sberbank.ru>
2023-10-09 15:18:08 +0300

    fish vars

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -2,5 +2,11 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+set -xU XDG_CONFIG_HOME $HOME/.config
+set -xU XDG_DATA_HOME $HOME/.local/share
+set -xU XDG_STATE_HOME $HOME/.local/state
+set -xU XDG_CACHE_HOME $HOME/.cache
+set -xU SPACEMACSDIR $XDG_CONFIG_HOME/spacemacs
+
 starship init fish | source
 
