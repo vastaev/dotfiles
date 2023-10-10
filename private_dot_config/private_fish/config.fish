189b8c51f1a080cf503f73b9257e37164a0c6b01
Вастаев Олег Сергеевич <osvastaev@sberbank.ru>
2023-10-10 12:13:44 +0300

    figured out fish vars

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -2,11 +2,21 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+#XDG vars
 set -xU XDG_CONFIG_HOME $HOME/.config
 set -xU XDG_DATA_HOME $HOME/.local/share
 set -xU XDG_STATE_HOME $HOME/.local/state
 set -xU XDG_CACHE_HOME $HOME/.cache
+
+#editor
+set -xU EDITOR "codium --wait"
+
+#spacemacs
 set -xU SPACEMACSDIR $XDG_CONFIG_HOME/spacemacs
 
+#starship launch
 starship init fish | source
 
+#asdf
+source /usr/local/opt/asdf/libexec/asdf.fish
+. ~/.asdf/plugins/java/set-java-home.fish
