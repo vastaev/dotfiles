d93e76448333d2ff9aaacf16d14a786f76c327d1
Вастаев Олег Сергеевич <osvastaev@sberbank.ru>
2023-11-20 11:40:09 +0300

    ignore local vars, direnv hook

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -1,19 +1,19 @@
-#XDG vars
+# XDG vars
 set -xU XDG_CONFIG_HOME $HOME/.config
 set -xU XDG_DATA_HOME $HOME/.local/share
 set -xU XDG_STATE_HOME $HOME/.local/state
 set -xU XDG_CACHE_HOME $HOME/.cache
 
-#editor
-set -xU EDITOR "codium --wait"
+# editor
+set -xU EDITOR "nvim"
 
-#spacemacs
+# spacemacs
 set -xU SPACEMACSDIR $XDG_CONFIG_HOME/spacemacs
 
-#starship launch
+# starship launch
 starship init fish | source
 
-#asdf
+# asdf
 source /usr/local/opt/asdf/libexec/asdf.fish
 . ~/.asdf/plugins/java/set-java-home.fish
 
@@ -21,3 +21,7 @@ if status is-interactive
   #auto-warpify
   printf '\eP$f{"hook": "SourcedRcFileForWarp", "value": { "shell": "fish"}}\x9c'
 end
+
+# direnv hook
+direnv hook fish | source
+
---
+++ private_dot_config/private_fish/functions/fish_greeting.fish
@@ -0,0 +1,3 @@
+function fish_greeting
+#	./Users/20058849/clj-stuff/randdoc.clj
+end
