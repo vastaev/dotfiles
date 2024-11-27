b7b1dc46735fb856a1b7d8e32b932fda0e62a131
Oleg V <olegvastaev@gmail.com>
2024-11-27 11:35:44 +0300

    small changes in gitsings and tmux keybindings

--- private_dot_config/nvim/lua/plugins/gitsigns.lua
+++ private_dot_config/nvim/lua/plugins/gitsigns.lua
@@ -1,4 +1,14 @@
 return {
     "lewis6991/gitsigns.nvim",
-    config = function() require("gitsigns").setup() end
+    config = function() 
+        require("gitsigns").setup {
+            signs = {
+                add = { text = '+' },
+                change = { text = '~' },
+                delete = { text = '_' },
+                topdelete = { text = '‾' },
+                changedelete = { text = '~' },
+            },
+        }
+    end
 }
