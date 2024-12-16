c850ece8c03becd876f8e3a963c72e43879fe467
Oleg V <olegvastaev@gmail.com>
2024-12-16 05:48:05 +0300

    trying lazyvim

---
+++ private_dot_config/nvim.bak/init.lua
@@ -0,0 +1,5 @@
+vim.deprecate = function() end
+require("config.set")
+require("config.remap")
+require("config.lazy")
+
--- private_dot_config/nvim/init.lua
+++ private_dot_config/nvim/init.lua
@@ -1,5 +1,2 @@
-vim.deprecate = function() end
-require("config.set")
-require("config.remap")
+-- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
-
